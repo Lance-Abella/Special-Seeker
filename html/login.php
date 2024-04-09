efe3fcd368f3184ba4dfe02cd39cebd1bd6b0a5e
evrylsem <evrylclaire.sembrino.21@usjr.edu.ph>
2024-04-09 19:37:52 +0800

    with fonts in css files

--- html/login.php
+++ html/login.php
@@ -9,12 +9,30 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="../images/logo-orig.png" type="image">
-        <!-- <link rel="stylesheet" href="../bootstrap.min.css"> -->
+        <link rel="stylesheet" href="../bootstrap.min.css">
         <link rel="stylesheet" href="../css/login.css" type="text/css">
         <title>Login</title>
     </head>
     <body>
-        <div class="img-container">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    img here
+                </div>
+                <div class="col login-form">
+                    <img src="../images/logo-orig.png" alt="">
+                    <label for="inputPassword5" class="form-label"></label>
+                    <input class="form-control" type="text" placeholder="Username" aria-label="default input example">
+                    <label for="inputPassword5" class="form-label"></label>
+                    <input type="password" id="inputPassword5" class="form-control" placeholder="Password" aria-describedby="passwordHelpBlock">
+                    <button type="button" class="btn btn-light">Login</button>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- <div id="img-container">
             <img src="../images/login-page.png" alt="">
         </div>
         <div class="form-container">
@@ -23,10 +41,9 @@
                     <img src="../images/logo-orig.png" alt="">
                 </div>
                 <div>
-
                 </div>
             </form>
-        </div>
-        <!-- <script src="../bootstrap.bundle.min.js"></script> -->
+        </div> -->
+        <script src="../bootstrap.bundle.min.js"></script>
     </body>
 </html>
